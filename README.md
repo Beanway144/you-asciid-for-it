74996b691de0d1806028cf05153d1884c176ddb1
Ian <ian.benway@gmail.com>
2022-03-20 18:56:34 -0400

    Image conver now up! 📷 -> @

--- README.md
+++ README.md
@@ -1,44 +1,6 @@
-# Image to Ascii Converter
-The goal of this program is to turn an input jpg or png into ascii art.
+# You @scii'd For It
+A simple program that converts an image into a copyable string of ASCII characters.
+![logo](logo.png)
+### How to Use
+You need to have Python and the [Pillow](https://pillow.readthedocs.io/en/stable/installation.html) library installed. Once installe, do `python3 converter.py`. You should be prompted to input a path (e.g. *~/Desktop/Images/image.png*), followed by a resize factor (for smaller resizes, try 25; for larger resizes, try 60ish).
 
-## Current Outlook (updated 3/13/2022:
-1. Load and resize image
- - [ ] Load image with Pillow
- - [ ] Resize image to ensure text file isn't too large
-
-2. Convert image to grayscale
- - [ ] Define ascii conversion list
- - [ ]  Convert existing image to grayscale by pixel
-
-3. Convert each grayscale pixel to corresponding ascii character
- - [ ] Convert individual grayscale images to ascii characters
-
-4. Output image
- - [ ] Print output image to screen
- - [ ] Set default csv file output repository
-
-________________________________________________________________________
-
-## Current Outlook:
-1. Take an input image (sehardwick)
-- [x] Ask the user for a path to file
-- [ ] Make sure the path leads to a jpg, png, etc.
-- [ ] Access image data
-- [ ] Put RGB data into data structure (like 2d list)
-
-2. Convert all image pixels to black and white (sehardwick)
-- [ ] Run through the matrix, taking the average of each R, B, and G
-- [ ] Replace RGB data with that average at each pixel
-
-3. Create a matrix of chars depending on RGB value at each pixel (beanway144)
-- [ ] Find average of all values
-- [ ] Find good range to determine relative strength of character density
-- [ ] Depending on resolution specs, get average of multiple pixels
-- [ ] Fill in matrix with chars like (space), -, =, $, &, #
-
-4. Print char matrix (beanway144)
-- [ ] Compile the matrix into strings
-- [ ] Print and finish
-
-## Design Considerations:
-- What should the resolution of the final ascii string be? should it be 1/4th the original?
--- converter.py
+++ converter.py
@@ -1,15 +1,18 @@
-import cv2
-import numpy as np
-#Pillow must be installed before following import will resolve: -SH
-import PIL.Image
+from PIL import Image
 
-
-# gets the image from the input path, or displays error
-# message if image cannot be found; -SH
-# dumps image data into approprate data structure
-# https://pythonspot.com/image-data-and-operations/
-
-#moved getFile() to main() -SH
+#@ # $ % ? * + ; : , .
+charDict = {
+    0.1: '.',
+    0.2: ':',
+    0.3: ';',
+    0.4: '+',
+    0.5: '*',
+    0.6: '?',
+    0.7: '%',
+    0.8: '$',
+    0.9: '#',
+    1.0: '@',
+}
 
 #use Pillow convert method with option L to
 #convert image to greyscale -SH
@@ -19,36 +22,74 @@ def decolorize(image):
 #resize image to limit text file size -SH
 def resize(image, n_width = 50):
     width, height = image.size
-    n_height = n_width * height / width
+    n_height = int(n_width * height / width)
     return image.resize((n_width, n_height))
 
 # initialize character matrix
-def initCharMat():
-    pass 
+def initCharMat(x, y):
+    return [[' ' for i in range(x)] for j in range(y)]
+
+# takes a float value and searches the dictionary for the
+#  first char with greater value
+def valToChar(val):
+    for i in charDict:
+        if val <= i:
+            return charDict[i]
+    print("Error: given bad val")
+    exit()
+
 
 # fill in character matrix according to image data
-def fillCharMat():
-    initCharMat()
-    pass 
+def fillCharMat(x, y, image):
+    mat = initCharMat(x, y)
+    px = image.load()
+    # first, get the highest valued pixel
+    maxPix = 0
+    for i in range(x):
+        for j in range(y):
+            if px[i, j] > maxPix:
+                maxPix = px[i, j]
+    
+    for i in range(0, y-1):
+        for j in range(0, x-1):
+            intensity = float(px[j, i] / maxPix)
+            c = valToChar(intensity)
+            mat[i][j] = c
+    return mat
+
+
 
 # print the matrix
-def printMatrix(mat):
-    pass
+def printMatrix(x, y, mat):
+    for j in range(0, y-1):
+        line = ""
+        for i in range(0, x-1):
+            line += mat[j][i]
+            line += mat[j][i]
+        print(line)
+
 
 def main():
     #previous getFile() function -SH    
     path = input("Enter the path to the image : \n")
+    image = 0
     try:
-        image = PIL.Image.open(path)
+        image = Image.open(path)
     except:
-        print(path, "Unable to finde image")
-        
-    #use resize variable to resize image
-    image = resize(image)
-
-    decolorize()
-    fillCharMat()
-    printMatrix()
+         print(path, "Unable to find image")
+         exit()
+    #get resize size
+    try:
+        size = int(input("Enter image resize (small~25; large~60): "))
+    except:
+        print("Size must be an integer.")
+        exit()
+    # change '25' to make different size
+    image = resize(image, size)
+    (x, y) = image.size
+    image = decolorize(image)
+    mat = fillCharMat(x, y, image)
+    printMatrix(x, y, mat)
 
 
 main()  
