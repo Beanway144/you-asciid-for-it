46568e68bac287fb36c30ab64b88e2048e3ff3c3
Ian <ian.benway@gmail.com>
2022-03-20 17:07:02 -0400

    sh's commit

--- converter.py
+++ converter.py
@@ -1,14 +1,26 @@
-from PIL import Image
-# gets the image from the input path, or throws errors if
-# path does not exist or is not appropriate file type;
+import cv2
+import numpy as np
+#Pillow must be installed before following import will resolve: -SH
+import PIL.Image
+
+
+# gets the image from the input path, or displays error
+# message if image cannot be found; -SH
 # dumps image data into approprate data structure
 # https://pythonspot.com/image-data-and-operations/
-def getFile(inp):
-    pass
 
-# turns the image into black and white
-def decolorize():
-    pass
+#moved getFile() to main() -SH
+
+#use Pillow convert method with option L to
+#convert image to greyscale -SH
+def decolorize(image):
+    return image.convert("L")
+
+#resize image to limit text file size -SH
+def resize(image, n_width = 50):
+    width, height = image.size
+    n_height = n_width * height / width
+    return image.resize((n_width, n_height))
 
 # initialize character matrix
 def initCharMat():
@@ -24,8 +36,16 @@ def printMatrix(mat):
     pass
 
 def main():
-    inp = input("Enter a file path: ")
-    getFile(inp)
+    #previous getFile() function -SH    
+    path = input("Enter the path to the image : \n")
+    try:
+        image = PIL.Image.open(path)
+    except:
+        print(path, "Unable to finde image")
+        
+    #use resize variable to resize image
+    image = resize(image)
+
     decolorize()
     fillCharMat()
     printMatrix()
